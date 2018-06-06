0c3c9cc8216fa53481d5527dfee118ea18f0f7b2
Roman Kislov <roman@kislovs.ru>
2018-06-06 22:38:06 +0500

    add index view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
 
+
 class App extends Component{
     constructor(props) {
         super(props)
@@ -11,11 +12,49 @@ class App extends Component{
         }
     }
     componentDidMount() {
-        axios.get('/api/books')
+        axios.get('/api/book')
             .then(res => {
-                this.state({books: res.data})
+                this.setState({books: res.data})
                 console.log(this.state.books)
             })
+    }
+    render(){
+        return (
+            <div class="container">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            Каталог
+                        </h3>
+                    </div>
+                    <div class="panel-body">
+                        <h4><Link to='/create'>
+                            <span class="fa fa-plus-circle" aria-hidden="true"></span>
+                            &nbsp; Добавить</Link></h4>
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>ISBN</th>
+                                    <th>Title</th>
+                                    <th>Author</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.state.books.map(book =>
+                                <tr>
+                                    <td>
+                                        <Link to={`/show/${book._id}`}>{book.isbn}</Link>
+                                    </td>
+                                    <td>{book.title}</td>
+                                    <td>{book.author}</td>
+                                </tr>
+                                )}
+                                </tbody>
+                            </table>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReavtDOM from 'react-dom'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './fa/css/fontawesome-all.min.css'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 import Edit from './components/Edit'
@@ -17,5 +18,7 @@ ReavtDOM.render(
             <Route path='/create' component={Create}/>
             <Route path='/show/:id' component={Show}/>
         </div>
-    </Router>
+    </Router>,
+    document.getElementById('root')
 )
+registerServiceWorker()
